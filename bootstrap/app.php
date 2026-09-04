ad242137f10012f769dd0ed8af5d014c77ebaf02
Vinicius Coutinho <vinicius@promofarma.com.br>
2026-09-04 15:04:24 -0300

    Update trusted proxies in app.php configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         $middleware->trustProxies(
-            at: ['*'],
+         $middleware->trustProxies(at: ['localhost', '*.promofarma.int']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
